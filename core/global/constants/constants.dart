13802c11cf919cf523b17894f29e178b17cdf59d
Rb2030 <rossmail2050@gmail.com>
2021-03-20 23:23:41 +0000

    Add theme, add fonts, add constants, add ui_helper

---
+++ core/global/constants/constants.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Constants extends InheritedWidget {
+  static Constants of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<Constants>();
+
+  const Constants({Widget child, Key key}) : super(key: key, child: child);
+
+// General
+
+  static const String crokett = 'Crokett';
+  static const String serverError = 'Server error, please try again later.';
+  static const String ok = 'OK';
+  static const String next = 'Next';
+  static const String submit = 'Submit';
+
+// Sign In Screen
+
+  static const String email = 'Email';
+  static const String password = 'Password';
+  static const String username = 'Username';
+  static const String signIn = 'Sign In';
+  static const String logIn = 'Log In';
+  static const String register = 'Register';
+  static const String forgottenSigninDetails = 'Forgotten sign In details?';
+  static const String invalidEmail = 'Invalid Email';
+  static const String invalidPassword = 'Invalid Password';
+  static const String invalidUsername = 'Invalid Username';
+  static const String emailAlreadyInUseTextFieldError = 'Email Already In Use';
+
+  static const String cancelledByUser = 'Cancelled by user';
+  static const String emailAlreadyInUse = 'Email already registered. Please sign in by pressing back, or if you wish to reset your password press "Forgotten sign in details?".';
+  static const String invalidUsernameAndPasswordCombo = 'Invalid username and password combination';
+  static const String pleaseTryAgain = 'Please try again';
+  static const String usernameTaken = 'This username has already been taken, please choose another unique one';
+
+// Register Screen
+
+  static const String emailInformation = 'Please ensure that you do not leave any blank spaces and your email is in the correct format e.g crokett@mail.com.';
+  static const String passwordInformation = 'Please ensure that the password has at least one capital letter, one number, one special character, no blank spaces and is at least 6 characters long.';
+  static const String usernameInformation = "Please select a username which is unique. This will be linked to your account so please refrain from using any profanity.";
+  static const String unableToRegister = "Unable to register";
+
+// Font 
+
+  static const String carinoSansFont = 'CarinoSans';
+
+  @override
+  bool updateShouldNotify(Constants oldWidget) => false;
+}
---
+++ core/global/helpers/ui_helper.dart
@@ -0,0 +1,28 @@
+
+class UIHelper {
+  // Spacing constants.
+
+  static const double spacer = 20.0;
+
+  static const double lineCornerRadius = 5.0;
+  static const double lineHeight = 0.8;
+  static const double paddingBetweenElements = 24.0;
+  static const double iconSize = 25.0;
+  static const double elevation = 8.0;
+  static const double appBarHeight = 65.0;
+
+  /// TextField
+
+  static const double textFieldBorderWidthDisabled = 0.8;
+  static const double textFieldBorderWidthFocused = 1.7;
+  static const double textFieldBorderRadius = 6;
+  static const double textFieldPadding = 10;
+  static const double textFieldHeight = 50;
+
+  /// Button
+
+  static const double buttonCornerRadius = 4;
+  static const double buttonMinHeight = 45;
+  static const double buttonMinWidth = 118;
+  static const double buttonPadding = 12;
+}

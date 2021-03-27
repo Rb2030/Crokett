74cc5eb3803e57f9befa633556fda68196b4ca8a
Rb2030 <rossmail2050@gmail.com>
2021-03-27 21:21:46 +0000

    Push before bringing in new navigation 2.0.

--- lib/core/global/hive/hive_objects.dart
+++ lib/core/global/hive/hive_objects.dart
@@ -2,9 +2,10 @@ import 'package:hive/hive.dart';
 
 @HiveType(typeId: 0)
 class User extends HiveObject {
-
   @HiveField(0)
-  String firstName = '';
+  String firstName;
   @HiveField(1)
-  String secondName = '';
+  String secondName;
+
+  User(this.firstName, this.secondName);
 }

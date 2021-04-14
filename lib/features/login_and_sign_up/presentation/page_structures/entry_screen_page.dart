4a71de0fa392c2efb3e95557c443734d7550929b
Rb2030 <rossmail2050@gmail.com>
2021-04-14 23:43:19 +0100

    Add entry screen page

--- lib/features/login_and_sign_up/presentation/page_structures/entry_screen_page.dart
+++ lib/features/login_and_sign_up/presentation/page_structures/entry_screen_page.dart
@@ -1,11 +1,16 @@
+import 'package:crokett/features/login_and_sign_up/application/entry_screen_bloc/entry_screen_bloc.dart';
+import 'package:crokett/injection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EntryScreenPage extends StatelessWidget {
-
-  const EntryScreenPage();
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => getIt<EntryScreenBloc>(),
+        child: //TODO: Put entry screen UI in here,
+    )
+    );
   }
 }

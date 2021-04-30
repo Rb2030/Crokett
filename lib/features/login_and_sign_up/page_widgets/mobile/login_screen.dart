0c9da1a15792a12c4796b2d4f80b47619ed219c6
Rb2030 <rossmail2050@gmail.com>
2021-04-30 14:20:15 +0100

    Add scrollView and bottom padding, top sized box

--- lib/features/login_and_sign_up/page_widgets/mobile/login_screen.dart
+++ lib/features/login_and_sign_up/page_widgets/mobile/login_screen.dart
@@ -7,7 +7,6 @@ import 'package:crokett/core/global/helpers/ui_helper.dart';
 import 'package:crokett/features/login_and_sign_up/blocs/login_bloc/login_bloc.dart';
 import 'package:crokett/features/login_and_sign_up/page_widgets/mobile/bottom_sheet_widget.dart';
 import 'package:crokett/injection.dart';
-import 'package:crokett/routes/crokett_configuration.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -37,121 +36,124 @@ class LoginScreen extends StatelessWidget {
           },
           child: Scaffold(
             backgroundColor: CustomColours.crokettYellow,
-            body: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: getDeviceType() == DeviceType.Phone
-                      ? rsc.rW(10)
-                      : rsc.rW(24)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  SizedBox(height: rsc.rH(16)),
-                  Row(
-                    children: [
-                      const Spacer(),
-                      Container(
-                          height: rsc.rH(30),
-                          width: rsc.rH(30),
-                          color: Colors.white),
-                      const Spacer(),
-                    ],
-                  ),
-                  SizedBox(height: rsc.rH(7)),
-                  Row(
-                    children: [
-                      const Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 1, 0, 0),
-                        child: Text(
-                          'cr',
-                          style: Theme.of(context).textTheme.headline1,
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getDeviceType() == DeviceType.Phone
+                        ? rsc.rW(10)
+                        : rsc.rW(24),
+                        vertical: rsc.rH(10)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: rsc.rH(6)),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        Container(
+                            height: rsc.rH(30),
+                            width: rsc.rH(30),
+                            color: Colors.white),
+                        const Spacer(),
+                      ],
+                    ),
+                    SizedBox(height: rsc.rH(7)),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 1, 0, 0),
+                          child: Text(
+                            'cr',
+                            style: Theme.of(context).textTheme.headline1,
+                          ),
                         ),
-                      ),
-                      const SizedBox(width: 1),
-                      Column(
-                        children: [
-                          SizedBox(height: 16),
-                          Container(
-                            height: 32,
-                            width: 32,
-                            child: RotationTransition(
-                              turns: AlwaysStoppedAnimation(52 / 360),
-                              child: SvgPicture.asset(Images.imageHobPower2),
+                        const SizedBox(width: 1),
+                        Column(
+                          children: [
+                            SizedBox(height: 16),
+                            Container(
+                              height: 32,
+                              width: 32,
+                              child: RotationTransition(
+                                turns: AlwaysStoppedAnimation(52 / 360),
+                                child: SvgPicture.asset(Images.imageHobPower2),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(width: 1),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 1, 0, 0),
-                        child: Text(
-                          'kett',
-                          style: Theme.of(context).textTheme.headline1,
+                          ],
                         ),
-                      ),
-                      const Spacer(),
-                    ],
-                  ),
-                  SizedBox(height: rsc.rH(7)),
-                  BlocProvider(
-                    lazy: false,
-                    create: (context) => getIt<LoginBloc>(),
-                    child: LoginButton(context),
-                  ),
-                  SizedBox(height: UIHelper.paddingBetweenElements),
-                  state is LoginStateInitial
-                      ? ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            onPrimary: Colors.black,
-                            primary: Colors.white,
+                        const SizedBox(width: 1),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 1, 0, 0),
+                          child: Text(
+                            'kett',
+                            style: Theme.of(context).textTheme.headline1,
                           ),
-                          onPressed: () {
-                            debugPrint('SignUp pressed');
-                          },
-                          child: Text(Constants.signUp),
-                        )
-                      : Container(),
-                  SizedBox(height: UIHelper.paddingBetweenElements),
-                  state is LoginStateInitial
-                      ? Row(children: [
-                          const Spacer(),
-                          Container(
-                            height: 3,
-                            width: rsc.rW(12),
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(2)),
+                        ),
+                        const Spacer(),
+                      ],
+                    ),
+                    SizedBox(height: rsc.rH(7)),
+                    BlocProvider(
+                      lazy: false,
+                      create: (context) => getIt<LoginBloc>(),
+                      child: LoginButton(context),
+                    ),
+                    SizedBox(height: UIHelper.paddingBetweenElements),
+                    state is LoginStateInitial
+                        ? ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              onPrimary: Colors.black,
+                              primary: Colors.white,
                             ),
-                          ),
-                          const Spacer()
-                        ])
-                      : Container(),
-                  SizedBox(height: UIHelper.paddingBetweenElements),
-                  state is LoginStateInitial
-                      ? ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            onPrimary: Colors.black,
-                            primary: Colors.white,
-                          ),
-                          onPressed: () {
-                            debugPrint('Sign In with Google pressed');
-                          },
-                          child: Row(
-                            children: [
-                              const Spacer(),
-                              Container(
-                                  height: UIHelper.iconSize,
-                                  width: UIHelper.iconSize,
-                                  child: SvgPicture.asset(Images.googleG)),
-                              SizedBox(width: UIHelper.buttonPadding),
-                              Text(Constants.signInWithGoogle),
-                              const Spacer(),
-                            ],
-                          ),
-                        )
-                      : Container(),
-                ],
+                            onPressed: () {
+                              debugPrint('SignUp pressed');
+                            },
+                            child: Text(Constants.signUp),
+                          )
+                        : Container(),
+                    SizedBox(height: UIHelper.paddingBetweenElements),
+                    state is LoginStateInitial
+                        ? Row(children: [
+                            const Spacer(),
+                            Container(
+                              height: 3,
+                              width: rsc.rW(12),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(2)),
+                              ),
+                            ),
+                            const Spacer()
+                          ])
+                        : Container(),
+                    SizedBox(height: UIHelper.paddingBetweenElements),
+                    state is LoginStateInitial
+                        ? ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              onPrimary: Colors.black,
+                              primary: Colors.white,
+                            ),
+                            onPressed: () {
+                              debugPrint('Sign In with Google pressed');
+                            },
+                            child: Row(
+                              children: [
+                                const Spacer(),
+                                Container(
+                                    height: UIHelper.iconSize,
+                                    width: UIHelper.iconSize,
+                                    child: SvgPicture.asset(Images.googleG)),
+                                SizedBox(width: UIHelper.buttonPadding),
+                                Text(Constants.signInWithGoogle),
+                                const Spacer(),
+                              ],
+                            ),
+                          )
+                        : Container(),
+                  ],
+                ),
               ),
             ),
           ),
